1b09bafcdf986c6bdc5902e8ea6266b89501ba8a
Scott Bergler and Tavish OKeefe <scott@>
2018-10-17 10:03:18 -0700

    add styling, form functionality, and letter text.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-  
+  $("#formOne").submit(function(event) {
+    var person1Input = $("input#person1").val();
+
+    $(".person1").append(person1Input);
+
+    $("#letter").removeClass("hide");
+    $("#formOne").addClass("hide");
+
+    event.preventDefault();
+  });
 });
